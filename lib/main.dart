54f4aecb0beec8717fefd79a3246a756483ad78d
DELL <snehal18122004@gmail.com>
2023-02-20 18:29:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:combo_2/home.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => home(),
+
+
+      },
+    )
+  );
+}
